9c0f9e2deb7a88186b8e60b33e9b7abddd906426
Christian Thompson <christian510@gmail.com>
2022-12-30 14:29:28 -0700

    create views of app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import LandingPageView from './views/LandingView.js/LandingPageView/Login';
+import LandingPageView from './views/LandingView.js/LandingPageView/LandingPageView';
 
-function App() {
-  return (
-    <div className="App-container">
-      <LandingPageView />
-      
-    </div>
-  );
+class App extends React.Component() {
+    constructor (props) {
+      super(props);
+      this.state = {        
+        isLoggedIn: false,
+        hasSession: false,
+      }
+
+    }
+    render() {
+
+      return (
+        <div className="App-container">
+          <LandingPageView />
+          {/* Login view */}
+          {/* About page view */}
+          {/* SavedWeatherView */}
+          {/* CurrentWeatherView */}
+          {/* WeatherMapView */}
+          
+        </div>
+      );
+    }
 }
 
 export default App;
