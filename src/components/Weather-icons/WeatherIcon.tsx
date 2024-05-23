22fd4765aa77ddbd225a0bc89be843825d0eed87
christian510@gmail.com <christian510@gmail.com>
2024-05-23 00:17:08 -0600

    write component, css, fonts, icon_map

---
+++ src/components/Weather-icons/WeatherIcon.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./WeatherIcon.css";
+import { weatherIcons } from "../../models/icon_map"; "../../models/icon_map";
+
+export interface WeatherIconProps {
+    isday: boolean;
+    icon: string;
+    description: string;
+}
+
+export default function WeatherIcon({ isday=true, icon="skc", description="Fair/clear" }: WeatherIconProps) {
+
+    const iconMap = weatherIcons.find((w) => w.icon === icon);
+    const iconMatch = isday ? iconMap?.reactIcon.day : iconMap?.reactIcon.night;
+
+    return (
+        <div>
+            <i className={`wi ${iconMatch}`} ></i>
+        </div>
+    );
+};
---
+++ src/models/icon_map.ts
@@ -0,0 +1,274 @@
+export const weatherIcons = [
+    {
+        icon: "skc",
+        description: "Fair/clear",
+        reactIcon: {
+            day: "wi-day-sunny",
+            night: "wi-night-clear"
+        }
+    },
+    {
+        icon: "few",
+        description: "A few clouds",
+        reactIcon: {
+            day: "wi-day-sunny-overcast",
+            night: "wi-night-alt-partly-cloudy"
+        }
+    },
+    {
+        icon: "sct",
+        description: "Partly cloudy",
+        reactIcon: {
+            day: "wi-day-sunny-overcast",
+            night: "wi-night-alt-partly-cloudy"
+        }
+    },
+    {
+        icon: "bkn",
+        description: "Mostly cloudy",
+        reactIcon: {
+            day: "wi-day-cloudy",
+            night: "wi-night-alt-cloudy"
+        }
+    },
+    {
+        icon: "ovc",
+        description: "Overcast",
+        reactIcon: {
+            day: "wi-day-cloudy",
+            night: "wi-night-alt-cloudy"
+        }
+    },
+    {
+        icon: "wind_skc",
+        description: "Fair/clear and windy",
+        reactIcon: {
+            day: "wi-day-windy",
+            night: "wi-windy"
+        }
+    },
+    {
+        icon: "wind_few",
+        description: "A few clouds and windy",
+        reactIcon: {
+            day: "wi-day-cloudy-windy",
+            night: "wi-night-alt-cloudy-windy"
+        }
+    },
+    {
+        icon: "wind_sct",
+        description: "Partly cloudy and windy",
+        reactIcon: {
+            day: "wi-day-cloudy-windy",
+            night: "wi-night-alt-cloudy-windy"
+        }
+    },
+    {
+        icon: "wind_bkn",
+        description: "Mostly cloudy and windy",
+        reactIcon: {
+            day: "wi-day-cloudy-windy",
+            night: "wi-night-alt-cloudy-windy"
+        }
+    },
+    {
+        icon: "wind_ovc",
+        description: "Overcast and windy",
+        reactIcon: {
+            day: "wi-day-cloudy-windy",
+            night: "wi-night-alt-cloudy-windy"
+        }
+    },
+    {
+        icon: "snow",
+        description: "Snow",
+        reactIcon: {
+            day: "wi-snow",
+            night: "wi-snow"
+        }
+    },
+    {
+        icon: "rain_snow",
+        description: "Rain/snow",
+        reactIcon: {
+            day: "wi-day-rain-mix",
+            night: "wi-night-alt-rain-mix"
+        }
+    },
+    {
+        icon: "rain_sleet",
+        description: "Rain/sleet",
+        reactIcon: {
+            day: "wi-day-rain-mix",
+            night: "wi-night-alt-rain-mix"
+        }
+    },
+    {
+        icon: "snow_sleet",
+        description: "Snow/sleet",
+        reactIcon: {
+            day: "wi-day-sleet",
+            night: "wi-night-alt-sleet"
+        }
+    },
+    {
+        icon: "fzra",
+        description: "Freezing rain",
+        reactIcon: {
+            day: "wi-day-sleet",
+            night: "wi-night-alt-sleet"
+        }
+    },
+    {
+        icon: "rain_fzra",
+        description: "Rain/freezing rain",
+        reactIcon: {
+            day: "wi-day-sleet",
+            night: "wi-night-alt-sleet"
+        }
+    },
+    {
+        icon: "snow_fzra",
+        description: "Freezing rain/snow",
+        reactIcon: {
+            day: "wi-day-sleet",
+            night: "wi-night-alt-sleet"
+        }
+    },
+    {
+        icon: "sleet",
+        description: "Sleet",
+        reactIcon: {
+            day: "wi-day-sleet",
+            night: "wi-night-alt-sleet"
+        }
+    },
+    {
+        icon: "rain",
+        description: "Rain",
+        reactIcon: {
+            day: "wi-day-rain",
+            night: "wi-night-alt-rain"
+        }
+    },
+    {
+        icon: "rain_showers",
+        description: "Rain showers (high cloud cover)",
+        reactIcon: {
+            day: "wi-day-showers",
+            night: "wi-night-alt-showers"
+        }
+    },
+    {
+        icon: "rain_showers_hi",
+        description: "Rain showers (low cloud cover)",
+        reactIcon: {
+            day: "wi-day-showers",
+            night: "wi-night-alt-showers"
+        }
+    },
+    {
+        icon: "tsra",
+        description: "Thunderstorm (high cloud cover)",
+        reactIcon: {
+            day: "wi-day-thunderstorm",
+            night: "wi-night-alt-thunderstorm"
+        }
+    },
+    {
+        icon: "tsra_sct",
+        description: "Thunderstorm (medium cloud cover)",
+        reactIcon: {
+            day: "wi-day-thunderstorm",
+            night: "wi-night-alt-thunderstorm"
+        }
+    },
+    {
+        icon: "tsra_hi",
+        description: "Thunderstorm (low cloud cover)",
+        reactIcon: {
+            day: "wi-day-thunderstorm",
+            night: "wi-night-alt-thunderstorm"
+        }
+    },
+    {
+        icon: "tornado",
+        description: "Tornado",
+        reactIcon: {
+            day: "wi-tornado",
+            night: "wi-tornado"
+        }
+    },
+    {
+        icon: "hurricane",
+        description: "Hurricane conditions",
+        reactIcon: {
+            day: "wi-hurricane",
+            night: "wi-hurricane"
+        }
+    },
+    {
+        icon: "tropical_storm",
+        description: "Tropical storm conditions",
+        reactIcon: {
+            day: "wi-storm-warning",
+            night: "wi-storm-warning"
+        }
+    },
+    {
+        icon: "dust",
+        description: "Dust",
+        reactIcon: {
+            day: "wi-dust",
+            night: "wi-dust"
+        }
+    },
+    {
+        icon: "smoke",
+        description: "Smoke",
+        reactIcon: {
+            day: "wi-smoke",
+            night: "wi-smoke"
+        }
+    },
+    {
+        icon: "haze",
+        description: "Haze",
+        reactIcon: {
+            day: "wi-day-haze",
+            night: "wi-night-fog"
+        }
+    },
+    {
+        icon: "hot",
+        description: "Hot",
+        reactIcon: {
+            day: "wi-hot",
+            night: "wi-hot"
+        }
+    },
+    {
+        icon: "cold",
+        description: "Cold",
+        reactIcon: {
+            day: "wi-snowflake-cold",
+            night: "wi-snowflake-cold"
+        }
+    },
+    {
+        icon: "blizzard",
+        description: "Blizzard",
+        reactIcon: {
+            day: "wi-snow-wind",
+            night: "wi-snow-wind"
+        }
+    },
+    {
+        icon: "fog",
+        description: "Fog/mist",
+        reactIcon: {
+            day: "wi-day-fog",
+            night: "wi-night-fog"
+        }
+    }
+];
