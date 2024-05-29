3b41e70a6dab571bde587b6c00d29874a0a66395
christian510@gmail.com <christian510@gmail.com>
2024-05-29 11:29:41 -0600

    fix color bug, add default values.

--- src/components/WeatherIcon/WeatherIcon.jsx
+++ src/components/WeatherIcon/WeatherIcon.jsx
@@ -3,26 +3,26 @@ import { weatherIcons } from "../../models/icon_map";
 import { useTheme } from "@mui/material";
 import Box from "@mui/material/Box";
 
-export default function WeatherIcon({ isday, icon, size, color, sx }){
+export default function WeatherIcon({ isday=true, icon='', size='sm', color='', sx=[] }){
     const theme = useTheme();
-    // console.log('theme: ', theme);
-    color = color ? color : theme.palette.grey[900];
     const sizes = [{"sm": "1em"}, {"med": "2em"}, {"lrg": "3em"}];
     const iconSize = sizes.find((s) => s[size] );
     // if (typeof size === 'number') iconSize = `${size.toString()}em`;
     const iconMap = weatherIcons.find((w) => w.icon === icon);
     const iconMatch = isday ? iconMap?.weatherIcon.day : iconMap?.weatherIcon.night;
+
     let styles = {
         fontSize: iconSize ? iconSize[size] : null,
-        color: color
+        color: color || theme.palette.common.white,
     };
-
     return (
         <Box
             display='flex'
             justifyContent='center'
             alignItems='center' 
-            sx={sx}
+            sx={[
+                ...(Array.isArray(sx) ? sx : [sx]),
+            ]}
             >
             <i className={`wi ${iconMatch}`} style={styles} ></i>
         </Box>
