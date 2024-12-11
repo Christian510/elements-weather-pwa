490de7688e1b6399de5810333282e9ada6b56b50
Christian Thompson <christian510@gmail.com>
2024-12-11 10:01:12 -0700

    creat list

---
+++ src/components/CreateList.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import ForecastCard from "../components/ForecastCard/ForecastCard";
+import WeatherIcon from "./WeatherIcon/WeatherIcon";
+import { parseUrl, formatDateTime } from '../utils/utl_functions';
+
+function dateTime(dt, type) {
+    let date;
+    if (type === 'hourly') {
+      date = dt.time;
+    }
+    if (type === 'daily') {
+      let split = dt.date.split(',');
+      date = split[0];
+    }
+    return date;
+}
+const CreateList = ({data, type, direction='row', count}) => {
+    // console.log('data: ', data.forecast[0]);
+    // console.log('type: ', type);
+    // console.log('count: ', count);
+    let current;
+    if (type === 'hourly') {
+      current = "Now"
+    }
+    else if (type === 'daily') {
+      current = "Today"
+    }
+    const forecast = data.forecast.map((item, index) => (
+      {
+          key: item.number,
+          title: data.location.name,
+          icon: parseUrl(item.icon),
+          forecast: item.shortForecast,
+          temp: item.temperature,
+          tempUnit: item.temperatureUnit,
+          isDaytime: item.isDaytime,
+          hour: index === 0 ? current : dateTime(formatDateTime(item.startTime, {hh: 'numeric'}), type),
+      }
+  ));
+// console.log('forecast: ', forecast);
+    return (
+        <>
+        {forecast.map((item, index) => {
+            while ( index <= count) {
+                return <ForecastCard
+                    key={index}
+                    content={{
+                        forecast: item.forecast,
+                        temp: { temp: item.temp, tempUnit: item.tempUnit },
+                        isDaytime: item.isDaytime,
+                        hour: item.hour
+                    }}
+                    flexDirection={direction}
+                    // justifyContent={justifyContent}
+                    // alignItems={alignItems}
+                    Icon={() => <WeatherIcon isDay={item.isDaytime} icon={item.icon} size="med" />}
+                    />
+            }
+        })}
+        </>
+);
+};
+
+export default CreateList
---
+++ src/utils/utl_functions.js
@@ -0,0 +1,104 @@
+// import { formatDateTime } from "../models/date";
+// CHECKS FOR RAIN OR SNOW
+const precip = {
+  precipitation: '',
+  type: '',
+}
+const checkPrecip = (rain, snow) => {
+  // let precipitation;
+  if (rain !== undefined) {
+    precip.precipitation = rain['1h'];
+    precip.type = "rain";
+  }
+  if (snow !== undefined) {
+    precip.precipitation = snow['1h'];
+    precip.type = "snow";
+  } else {
+    precip.precipitation = '0';
+    // precip.type = 'none';
+  }
+  return precip;
+}
+
+
+// RETURNS AN ADDRESS OBJECT USED RETRIEVE WEATHER DATA
+const addr = {
+  city: '',
+  state: '',
+  country: '',
+  abbr: ''
+};
+
+const validateAdr = str => {
+  let splitStr = str.trim().split(", ").filter(elm => elm !== '');
+  addr.city = splitStr[0].split(' ').map(elm => {
+    return elm.charAt(0).toUpperCase() + elm.slice(1);
+  }).join(' ');
+  if (splitStr.length < 2) {
+    // message sent to user to include a state or country
+    console.log("state, provice, or country required!");
+    return null;
+  }
+  if (splitStr.length === 2) {
+    // Validate for just a city, state/country
+    addr.abbr = splitStr[1].toUpperCase();
+  }
+  else if (splitStr.length === 3) {
+    // Validates for city, state, country
+    addr.state = splitStr[1].toUpperCase();
+    addr.country = splitStr[2].toUpperCase();
+  }
+  return addr;
+}
+
+export function parseUrl(url) {
+  const nUrl = new URL(url);
+  const path = nUrl.pathname.split('/');
+  const iconClass = path[path.length - 1].split(',')[0];
+  // console.log('icon: ', iconClass);
+  return iconClass;
+}
+
+export function formatDateStr(date) {
+  let dateStr='';
+  let operator = '';
+  if (date.includes('T')) {
+    const split = date.split('T');
+    const d = split[0];
+    let t = split[1];
+    if (t.at(-6) === '+' || t.at(-6) === '-') {
+      operator = t.at(-6);
+      t = t.split(operator)[0];
+      // console.log('t: ', t);
+    }
+    dateStr = `${d} ${t}`;
+  } else {
+    dateStr = null;
+  }
+  return dateStr;
+}
+
+export function formatDateTime(dt, { w='short', y='numeric', m='short', d='numeric', hh, mm, ss, tz}={}) {
+  // console.log('date str: ', formatDateStr(dt));
+  let dateTime = {};
+  if (formatDateStr(dt) !== null) {
+    dt = formatDateStr(dt);
+    // console.log('dt: ', dt);
+  }
+  const newdate = new Date(dt);
+  const dateOptions = {
+    year: y,
+    month: m,
+    weekday: w,
+    day: d,
+  };
+  const timeOptions = {
+    hour: hh,
+    minute: mm,
+    seconds: ss,
+    timezone: tz,
+  }
+  dateTime.date = newdate.toLocaleDateString('en-US', dateOptions)
+  dateTime.time = newdate.toLocaleTimeString('en-US', timeOptions);
+  return dateTime;
+}
