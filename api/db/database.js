31cfb52bf8ac5b40cf47b6edcbaa244db8b66d6d
Christian Thompson <christian510@gmail.com>
2025-01-30 10:21:52 -0700

    db clean up

--- api/db/database.js
+++ api/db/database.js
@@ -23,21 +23,6 @@ function executeQuery(query, values) {
     });
 }
 
-// if no Locations table exits create one.
-// async function createLocationsTable() {
-//     try {
-//         const result = await pool.query(`
-//         CREATE TABLE IF NOT EXISTS locations 
-//         (location_id VARCHAR(255) PRIMARY KEY, session_id VARCHAR(255))`);
-//         console.log('result: ', result[0]);
-//         pool.end();
-//         return result[0];
-//       } catch (error) {
-//         console.error('Error creating locations table:', error);
-//         throw error; // Re-throw to handle it appropriately in calling code
-//       }
-// }
-
 // Get Locations
 
 // async function getSessions() {
@@ -104,15 +89,20 @@ async function insertOne(params = null) {
   const [sf] = await executeQuery(sf_query, [params.location_id, params.session_id])
   const l_query = `SELECT * FROM locations WHERE location_id = ?;`;
   const [location] = await executeQuery(l_query, [params.location_id])
-
   const sf_insert_query = `INSERT INTO session_favorites (s_id, l_id, l_name) VALUES (?, ?, ?)`;
 
   if (sf.length < 1 && location.length < 1) {
-    const [sf] = await executeQuery(sf_insert_query, [params.session_id, params.location_id, params.name]);
+    const [sf] = await executeQuery(
+      sf_insert_query, 
+      [params.session_id, params.location_id, params.name]
+    );
     const l_insert_query = `
-                    INSERT INTO locations (location_id, name, state, country_code, lat, lng)
-                    VALUES (?, ?, ?, ?, ?, ?)`;
-    const [loc] = await executeQuery(l_insert_query, [params.location_id, params.name, params.state, params.country_code, params.lat, params.lng]);
+      INSERT INTO locations (location_id, name, state, country_code, lat, lng)
+      VALUES (?, ?, ?, ?, ?, ?)`;
+    const [loc] = await executeQuery(
+      l_insert_query, 
+      [params.location_id, params.name, params.state, params.country_code, params.lat, params.lng]
+    );
     if (sf.affectedRows === 0 || loc.affectedRows === 0) result = 0; // Message: 'db session_favorites not saved'
     if (sf.affectedRows === 1 && loc.affectedRows === 1) result = 1;
   }
