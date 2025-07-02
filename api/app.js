b9500a113e206a9c5b2888f30b744e4f44c0c2be
Christian Thompson <christian510@gmail.com>
2025-07-02 10:30:29 -0600

    debug build path in app.js

--- api/app.js
+++ api/app.js
@@ -71,7 +71,7 @@ app.get('/test', (req, res) => {
 
 // Serve static files from React's build folder in production/staging
   app.use(express.static(path.join(__dirname, "build")));
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
       console.log("path: ", path.join(__dirname, "build", "index.html"));
       res.sendFile(path.join(__dirname, "build", "index.html"));
   });
