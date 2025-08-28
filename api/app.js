266d548ea661ab42cde8c96bc9df2d4d6edbd7a4
Christian Thompson <christian510@gmail.com>
2025-08-28 09:28:21 -0600

    change domain to be .elementsweather.com

--- api/app.js
+++ api/app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const path = require('path');
-// const { fileURLToPath } = require('url');
 const sqlformat = require('./logger.js');
 const favoritesRouter = require('./routes/favorites.js');
 const redisStore = require('./redisStore.js');
@@ -48,7 +47,7 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     secure: false, // true if using https
-    domain: '.localhost',
+    domain: '.elementsweather.com',
     // maxAge: 1000 * 60 * 60 * 24 * 90, // 90 days
     maxAge: 1000 * 60 * 60 * 24 * 1, // 1 day
     sameSite: 'lax', // none, lax, strict
