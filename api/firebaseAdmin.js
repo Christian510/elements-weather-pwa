ddffabc90d792760ff5b1220c81987abf97c69d5
Christian Thompson <christian510@gmail.com>
2026-05-18 17:41:43 -0600

    update: hide secret fix.

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -1,7 +1,9 @@
 const admin = require('firebase-admin');
 
 if (!admin.apps.length) {
-  const serviceAccount = require('./serviceAccountKey.json');
+  const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
+    ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+    : require('./serviceAccountKey.json');
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
